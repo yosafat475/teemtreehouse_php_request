fe60d8f9603f3a8633d2dd8a40ec10b3ad8a9d8b
yosafat475 <70566441+yosafat475@users.noreply.github.com>
2020-10-12 16:12:04 +0700

    Add files via upload

---
+++ teemtreehouse_rquest/request/request.php
@@ -0,0 +1,21 @@
+<?php
+
+$data =[];
+
+if(isset($_GET['action'])){
+    $action=$_GET['action'];
+    if($action=='Daftar'){
+        $data[]=$_GET;
+    }
+}
+
+header("Content-Type:application/json");
+header("Access-Control-Allow-Origin:*");
+
+// var_dump($data);
+
+// var_dump(json_encode($data));
+
+echo json_encode($data);
+
+?>
